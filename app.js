5fa3338eeb2e60c8bcb7e147f6fbb4ee120ef3af
rebelde1231142 <leonelbrenodasilvagithub@gmail.com>
2025-02-18 08:39:00 -0300

    update3
    3

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
 
 app.get('/',(req,res)=>{
-    return res.status(200).json();
+    const resposta = {"Result:":"API em execução"
+    }
+    return res.status(200).json(Resposta);
+});
+
+app.get('/soma',(req,res)=>{
+    const {valores} = req.body;
+    let Result = valores.reduce((total,valor)=>total+valor, 0)
+    return res.status(200).json({"result":result})
+})
+
+const PORT = 3000;
+app.listen(PORT,()=>{
+    console.log(`aplicação esta em execução\nhttp://localhost:${PORT}`);
 });
